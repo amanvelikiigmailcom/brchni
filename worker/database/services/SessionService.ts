8b31e93dcd6b1a07fd89035903cac42d79ccdad8
admin <admin@MacBook-Pro-Anara.local>
2026-05-30 19:12:06 +0500

    feat: extend session TTL to 5 months

--- worker/database/services/SessionService.ts
+++ worker/database/services/SessionService.ts
@@ -31,7 +31,7 @@ interface SessionConfig {
 export class SessionService extends BaseService {
     static readonly config: SessionConfig = {
         maxSessions: 5,
-        sessionTTL: 3 * 24 * 60 * 60,
+        sessionTTL: 5 * 30 * 24 * 60 * 60,
         cleanupInterval: 60 * 60, // 1 hour
         maxConcurrentDevices: 3, // Max 3 devices concurrently
     };
